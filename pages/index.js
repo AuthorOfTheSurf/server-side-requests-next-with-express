0c052fcd2893d1d6eb0d5e99569e0b2b0c9efa36
Zane Kansil <zane@videoamp.com>
2018-04-23 10:34:46 -0700

    Remove 'a' and 'b' page examples

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import Link from 'next/link'
 
 export default () => (
   <ul>
-    <li><Link href='/b' as='/a'><a>a</a></Link></li>
-    <li><Link href='/a' as='/b'><a>b</a></Link></li>
     <li>
       <Link
         href={{pathname: '/posts', query: { id: '2' }}}
--- server.js
+++ server.js
@@ -10,14 +10,6 @@ app.prepare()
 .then(() => {
   const server = express()
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
-
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/a', req.query)
-  })
-
   server.get('/posts/:id', (req, res) => {
     return app.render(req, res, '/posts', { id: req.params.id })
   })
